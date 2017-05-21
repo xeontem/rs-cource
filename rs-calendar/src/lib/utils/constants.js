e1c59931473cde81c642fce692c2df5fceea148c
xeontem <xeontem@gmail.com>
2017-05-22 00:10:20 +0300

    rs-calendar/feature: prepare for development

---
+++ rs-calendar/src/lib/utils/constants.js
@@ -0,0 +1,16 @@
+
+export let navigate = {
+  PREVIOUS:  'PREV',
+  NEXT: 'NEXT',
+  TODAY: 'TODAY',
+  DATE: 'DATE'
+}
+
+
+export let views = {
+  MONTH: 'month',
+  WEEK: 'week',
+  WORK_WEEK: 'work_week',
+  DAY: 'day',
+  AGENDA: 'agenda'
+}
