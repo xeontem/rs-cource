a8a4d855f4b2f7e9eb20aade44e359dd924b23af
xeontem <xeontem@gmail.com>
2017-07-12 23:40:30 +0300

    accessibility/feature: implement accessibility

--- forms_and_widgets/js/main.js
+++ forms_and_widgets/js/main.js
@@ -1,3 +1,13 @@
+// -------------------------- inputs -----------------------------------------------
+let div = document.querySelector('label[for=file] div');
+let inputFile = document.querySelector('#file');
+div.addEventListener('mouseout', showSelectedFile);
+
+function showSelectedFile() {
+    let Subject = inputFile.value.length ? inputFile.value : 'Subject';
+    div.innerHTML = '<p class="placeholder-text">' + Subject + '</p><i class="fa fa-paperclip" aria-hidden="true"></i>';
+}
+
 let removeParticipantButton = document.querySelector('.email-container i');
 removeParticipantButton.addEventListener('click', removeParticipant);
 
@@ -12,19 +22,6 @@ function addParticipant() {
     document.querySelector('.email-container-component').style.display = 'block';
 }
 
-let likesComponent = document.querySelector('.likes');
-likesComponent.addEventListener('click', incrementLikes);
-
-function incrementLikes() {
-   document.querySelector('.likes p').innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i>' + (Number(document.querySelector('.likes p').innerText)+1);
-}
-
-let commentsComponent = document.querySelector('.comments');
-commentsComponent.addEventListener('click', incrementComments);
-
-function incrementComments() {
-   document.querySelector('.comments p').innerHTML = '<i class="fa fa-comment" aria-hidden="true"></i>' + (Number(document.querySelector('.comments p').innerText)+1);
-}
 
 //---------------------------------------------- diagram -----------------------------
 let heights = [48, 61, 118, 157, 128, 168, 142];
@@ -32,7 +29,6 @@ let columns =  document.querySelectorAll('.column-component');
 
 function setHeights() {
     [].map.call(columns, (column, i) => {
-    console.log(column.style.height);
     column.style.height = heights[i]+'px';
 });
 }
@@ -52,6 +48,31 @@ function selectCurrent() {
 }));
 this.classList.add('current');
 }
+
+let likes = comments = 0;
+
+let likesComponent = document.querySelector('.likes');
+likesComponent.addEventListener('click', incrementLikes);
+
+function incrementLikes() {
+   if(!(likes%2)) {
+       document.querySelector('.likes p').innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i>' + (Number(document.querySelector('.likes p').innerText)+1);
+       document.querySelector('.likes i').style.color='#fb6362'; 
+   } else document.querySelector('.likes p').innerHTML = '<i class="fa fa-heart" aria-hidden="true"></i>' + (Number(document.querySelector('.likes p').innerText)-1);
+   likes++; 
+}
+
+let commentsComponent = document.querySelector('.comments');
+commentsComponent.addEventListener('click', incrementComments);
+
+function incrementComments() {
+   if(!(comments%2)) {
+       document.querySelector('.comments p').innerHTML = '<i class="fa fa-comment" aria-hidden="true"></i>' + (Number(document.querySelector('.comments p').innerText)+1);
+       document.querySelector('.comments i').style.color='#fb6362'; 
+   } else document.querySelector('.comments p').innerHTML = '<i class="fa fa-comment" aria-hidden="true"></i>' + (Number(document.querySelector('.comments p').innerText)-1);
+   comments++; 
+}
+
 
 //-------------------------- calendar -------------------
 let monthes = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
